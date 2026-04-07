8f948b9fd80ddee51d61afd60b7009db29741dc9
michxz312 <michealeafaustin@gmail.com>
2026-04-07 00:54:26 -0700

    Added insert_employee function to db.py

--- scheduling-project/db.py
+++ scheduling-project/db.py
@@ -1,14 +1,23 @@
 import pymysql
 
 
-connection = pymysql(
-    host="localhost",
-    db="mydb",
-    user="root",
-    password="",
-    charset="utf8",
-    cursorclass = pymysql.cursors.DictCursor
-)
+def create_connection():
+    return pymysql.connect(
+        host="localhost",
+        db="mydb",
+        user="root",
+        password="",
+        charset="utf8",
+        cursorclass = pymysql.cursors.DictCursor
+    )
 
-cursor = connection.cursor()
-
+def insert_employees(employee):
+    con = create_connection()
+    cursor = con.cursor()
+    sql = "INSERT INTO employees (skill_level, training, salary) INTO VALUES(%s,%s,%s,%s)"
+    for e in employee:
+        e["training_level"] = ','.join(e['training_level'])
+    cursor.execute(sql, (employee))
+    con.commit()
+    cursor.close()
+    con.close()
---
+++ scheduling-project/seed.py
@@ -0,0 +1,4 @@
+from config import skill_levels, tasks_with_min_levels, distribution, salary_weight, days, demand_distribution, day_label, shift_label, enumerate_shift
+from utils import generate_employee_data_custom_distribution, generate_customer_data_custom_distribution
+from db import insert_employees, insert_customer
+
