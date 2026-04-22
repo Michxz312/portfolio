a0d4f696a4f0717bf70c14923e770cd3ca8b5e34
michxz312 <michealeafaustin@gmail.com>
2026-04-22 10:28:33 -0700

    Change layout for result.html

--- static/result.js
+++ static/result.js
@@ -14,48 +14,45 @@ document.addEventListener("DOMContentLoaded", () => {
     assignment.forEach(row => {
         const {day, id, shift} = row;
 
-        if (!schedule[day]) schedule[day] = {};
+        if (!schedule[id]) schedule[id] = {};
 
-        if (!schedule[day][shift]) schedule[day][shift] = [];
+        if (!schedule[id][day]) schedule[id][day] = [];
 
-        schedule[day][shift].push(id)
+        schedule[id][day].push(shift)
     });
+    
+    printEmployees(schedule);
+    
 
-    // create shift header
-    const shifts = ["Account opening",
-        "Credit card application",
-        "Loan Application",
-        "Mortgage Consultation",
-        "Retirement planning",
-        "Financial advising",
-        "Wealth management"
-    ];
+});
+
+function printEmployees(schedule) {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
     const headerRow = document.getElementById("schedule-header")
-    shifts.forEach(shift => {
+    days.forEach(day => {
         const th = document.createElement("th");
-        th.textContent = shift;
+        th.textContent = day;
         headerRow.appendChild(th);
     })
 
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-
     const tbody = document.getElementById("schedule-body")
-    days.forEach(day => {
+    const e_id = Object.keys(schedule)
+    e_id.forEach(id => {
         const tr = document.createElement("tr");
 
         // first column
-        const day_label = document.createElement("td");
-        day_label.textContent = day;
-        tr.appendChild(day_label);
+        const id_col = document.createElement("td");
+        id_col.textContent = id
+        tr.appendChild(id_col);
 
-        // insert employees
-        shifts.forEach(shift => {
+        // insert shifts
+        days.forEach(day => {
             const td = document.createElement("td")
-            const employees = schedule?.[day]?.[shift] || [];
+            const employees = schedule?.[id]?.[day] || [];
             td.textContent = employees.join(", ");
             tr.appendChild(td);
         });
         tbody.appendChild(tr)
     });
 
-});
+};
