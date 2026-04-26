a2a74c6eac4aa5598fd9e1c3ed3d1ee20be15dc2
michxz312 <michealeafaustin@gmail.com>
2026-04-25 23:04:45 -0700

    Add routes for input and result pages

--- assignment_project/routes.py
+++ assignment_project/routes.py
@@ -4,3 +4,11 @@ from . import assignment_bp
 @assignment_bp.route('/')
 def intro_page():
     return render_template('projects/assignment/intro.html')
+
+@assignment_bp.route('/input', methods=["GET", "POST"])
+def input_page():
+    return render_template('projects/assignment/input.html')
+
+@assignment_bp.route('/result', methods=["GET", "POST"])
+def result_page():
+    return render_template('projects/assignment/result.html')
