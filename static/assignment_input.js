f75cbf24b16ce50e6dfe819043cfae7df95e5d19
michxz312 <michealeafaustin@gmail.com>
2026-04-28 23:38:13 -0700

    Add gender distribution chart to assignment input page

--- static/assignment_input.js
+++ static/assignment_input.js
@@ -1,12 +1,25 @@
 const internationalCount = students.filter(s => s.international == 1).length;
 const domesticCount = students.length - internationalCount;
 
+const female = students.filter(s => s.gender == 1).length;
+const male = students.length - female;
+
 new Chart(document.getElementById('international_chart'), {
     type: 'pie',
     data: {
         labels: ['International', 'Domestic'],
         datasets: [{
             data: [internationalCount, domesticCount]
+        }]
+    }
+})
+
+new Chart(document.getElementById('gender_chart'), {
+    type: 'pie',
+    data: {
+        labels: ['Female', 'Male'],
+        datasets: [{
+            data: [female, male]
         }]
     }
 })
