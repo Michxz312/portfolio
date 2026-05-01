2561fe98df3a83ac479a0270c1485b77ccdca30b
michxz312 <michealeafaustin@gmail.com>
2026-04-30 22:56:03 -0700

    Implement get_results function

--- assignment_project/utils.py
+++ assignment_project/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import cvxpy as cp
 
 def create_courses(m, l, u):
     courses = []
@@ -33,5 +34,24 @@ def create_students(n, m):
     return students
 
 def get_results(students, courses):
-    data=[]
-    return data
+    n = len(students)
+    m = len(courses)
+    x = cp.Variable((n,m), integer = True)
+    pref = [list(map(int, s['preference'].split(","))) for s in students]
+    constraints = [] 
+
+    constraints += constraint1(x, constraints, n, pref)
+    constraints += [x >= 0, x <= 1]
+    
+    a = 2
+    objective = cp.Minimize(cp.sum([cp.sum([x[i, pref[i,0]]  + a*x[i, pref[i,1]] + a**2*x[i,pref[i,2]] + a**3*x[i,pref[i,3]]for i in range(n)])]))
+
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    return x.value.tolist()
+
+# Each student gets exactly one course j in preference of student i
+def constraint1(x, constraints, n, pref): 
+    for i in range(n):
+        constraints += [cp.sum([x[i,j] for j in pref[i]]) == 1]
+    return constraints
