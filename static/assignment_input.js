2d06329c18fa19bf857e0f0d63bd205290e877db
michxz312 <michealeafaustin@gmail.com>
2026-04-30 23:59:55 -0700

    Update form layout css

--- static/assignment_input.js
+++ static/assignment_input.js
@@ -1,78 +1,100 @@
-const internationalCount = students.filter(s => s.international == 1).length;
-const domesticCount = students.length - internationalCount;
+document.addEventListener("DOMContentLoaded", function() {
+    const internationalChart = document.getElementById('international_chart');
+    const genderChart = document.getElementById('gender_chart');
+    const preferenceChart = document.getElementById('preference_chart');
 
-new Chart(document.getElementById('international_chart'), {
-    type: 'pie',
-    data: {
-        labels: ['International', 'Domestic'],
-        datasets: [{
-            data: [internationalCount, domesticCount]
-        }]
-    }
-})
+    const internationalCount = students.filter(s => s.international == 1).length;
+    const domesticCount = students.length - internationalCount;
 
-const female = students.filter(s => s.gender == 1).length;
-const male = students.length - female;
-
-new Chart(document.getElementById('gender_chart'), {
-    type: 'pie',
-    data: {
-        labels: ['Female', 'Male'],
-        datasets: [{
-            data: [female, male]
-        }]
-    }
-})
-
-const rankCounts = {
-    1: {},
-    2: {},
-    3: {},
-    4: {}
-};
-
-students.forEach(s => {
-    const p = s.preference.split(",").map(Number);
-    p.forEach((course, rank) => {
-        rankCounts[rank+1][course] = (rankCounts[rank+1][course] || 0) + 1;
+    new Chart(document.getElementById('international_chart'), {
+        type: 'pie',
+        data: {
+            labels: ['International', 'Domestic'],
+            datasets: [{
+                data: [internationalCount, domesticCount]
+            }],
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
     })
-});
 
-const courses_label = courses.map(c => c.id);
-const datasets = courses_label.map(c => ({
-    label: c,
-    data: [1,2,3,4].map(r =>
-        rankCounts[r][c] || 0
-    )
-}));
+    const female = students.filter(s => s.gender == 1).length;
+    const male = students.length - female;
 
-new Chart(document.getElementById('preference_chart'), {
-    type: 'bar',
-    data: {
-        labels: ['1st', '2nd', '3rd', '4th'],
-        datasets: datasets
-    },
-    options: {
-        indexAxis: 'y',
-        scales: {
-            x: { stacked: true },
-            y: { stacked: true }
+    new Chart(document.getElementById('gender_chart'), {
+        type: 'pie',
+        data: {
+            labels: ['Female', 'Male'],
+            datasets: [{
+                data: [female, male]
+            }],
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    })
+
+    const rankCounts = {
+        1: {},
+        2: {},
+        3: {},
+        4: {}
+    };
+
+    students.forEach(s => {
+        const p = s.preference.split(",").map(Number);
+        p.forEach((course, rank) => {
+            rankCounts[rank+1][course] = (rankCounts[rank+1][course] || 0) + 1;
+        })
+    });
+
+    const courses_label = courses.map(c => c.id);
+    const datasets = courses_label.map(c => ({
+        label: c,
+        data: [1,2,3,4].map(r =>
+            rankCounts[r][c] || 0
+        )
+    }));
+
+    new Chart(document.getElementById('preference_chart'), {
+        type: 'bar',
+        data: {
+            labels: ['1st', '2nd', '3rd', '4th'],
+            datasets: datasets
         },
-        plugins: {
-            tooltip: {
-                callbacks: {
-                    label: function(tooltipItem) {
-                        const dataset = tooltipItem.dataset;
-                        const courseId = dataset.label;
-                        const count = tooltipItem.raw;
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            indexAxis: 'y',
+            scales: {
+                x: { stacked: true },
+                y: { stacked: true }
+            },
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                            const dataset = tooltipItem.dataset;
+                            const courseId = dataset.label;
+                            const count = tooltipItem.raw;
 
-                        return 'Course ID: '+ courseId + ', Count: ' + count;
+                            return 'Course ID: '+ courseId + ', Count: ' + count;
+                        }
                     }
                 }
             }
-        }
-    },
+        },
+    })
+
+    window.addEventListener('resize', function() {
+        
+    })
 })
+
+
 
 
 document.getElementById("result").addEventListener("click", async() => {
