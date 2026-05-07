3d41864a99bb45563b2e7f40986738e5f991325f
michxz312 <michealeafaustin@gmail.com>
2026-05-06 22:08:26 -0700

    Add API route for kanji data

--- kanji_game/routes.py
+++ kanji_game/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import kanji_game_bp
 
-@kanji_game_bp.route('/')
+@kanji_game_bp.route('/', methods=["GET"])
 def kanji():
     return render_template('projects/kanji/kanji.html')
+
+@kanji_game_bp.route('/api', methods=["POST"])
+def kanji_api():
+    data = request.get_json()
+    return jsonify(data)
--- static/kanji_app.js
+++ static/kanji_app.js
@@ -1,7 +1,18 @@
 async function loadKanji() {
     const res = await fetch('https://kanjiapi.dev/v1/kanji/jlpt-2')
     const vocab = await res.json();
-    console.log(vocab)
+    sendKanji(vocab)
+}
+
+async function sendKanji(data) {
+    const res = await fetch("/kanji/api", {
+        method:"POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+    console.log(data)
 }
 
 loadKanji();
