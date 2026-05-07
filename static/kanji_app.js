a665463966f33a59b023a340a0179f37bb442740
michxz312 <michealeafaustin@gmail.com>
2026-05-06 21:57:35 -0700

    Implement loadKanji function to fetch and log JLPT-2 kanji data

--- static/kanji_app.js
+++ static/kanji_app.js
@@ -1,1 +1,7 @@
-console.log('loaded')
+async function loadKanji() {
+    const res = await fetch('https://kanjiapi.dev/v1/kanji/jlpt-2')
+    const vocab = await res.json();
+    console.log(vocab)
+}
+
+loadKanji();
