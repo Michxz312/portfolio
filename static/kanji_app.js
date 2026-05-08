37946514e4ae0fabb09430825ab838d0d52bbc25
michxz312 <michealeafaustin@gmail.com>
2026-05-07 22:35:53 -0700

    Refactor getRandomKanjiWords function to fetch and return JLPT-N2 kanji words

--- static/kanji_app.js
+++ static/kanji_app.js
@@ -1,8 +1,21 @@
-async function loadKanji() {
+async function getRandomKanjiWords() {
     const res = await fetch('https://kanjiapi.dev/v1/kanji/jlpt-2')
     const vocab = await res.json();
-    const result = await sendKanji(vocab);
-    return result;
+    const kanjiList = await sendKanji(vocab);
+
+    let n2; 
+    while (true) {
+        const randomKanji = kanjiList[Math.floor(Math.random() * kanjiList.length)];
+        
+        const wordRes = await fetch(`/kanji/jisho/${randomKanji}`);
+        const wordData = await wordRes.json();
+        n2 = wordData.data.filter(kanji => kanji.jlpt && kanji.jlpt[0] === "jlpt-n2")
+
+        if (n2.length > 1) {
+            break;
+        }
+    }
+    return n2;
 }
 
 async function sendKanji(data) {
@@ -17,24 +30,5 @@ async function sendKanji(data) {
     return result;
 }
 
-async function getRandomWords(kanji) {
-    const res = await fetch(`/kanji/jisho/${kanji}`);
-    const data = await res.json();
-    console.log(data)
-    return data.data.filter(item =>
-        item.japanese.some(j => j.word && j.word.includes(kanji))
-    );
-    
-}
-
-async function quizKanji() {
-    const kanjiList = JSON.parse(localStorage.getItem("result"));
-    const randomIndex = Math.floor(Math.random() * kanjiList.length);
-    const kanji = await getRandomWords(kanjiList[randomIndex]);
-    console.log(kanji)
-    
-}
-
-loadKanji();
-
-quizKanji();
+const n2Words = getRandomKanjiWords();
+console.log(n2Words)
