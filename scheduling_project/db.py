e0b2e6e55a5c9ec6ec2095a3f7453cf772808929
michxz312 <michealeafaustin@gmail.com>
2026-06-07 20:42:50 -0700

    Add port configuration to database connection

--- scheduling_project/db.py
+++ scheduling_project/db.py
@@ -5,6 +5,7 @@ def create_connection():
     return pymysql.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
+        port=int(os.getenv("DB_PORT")),
         database=os.getenv("DB_NAME"),
         password=os.getenv("MYSQL_PASSWORD"),
         charset="utf8mb4",
